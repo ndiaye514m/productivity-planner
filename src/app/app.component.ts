942d38410b18c5b51a3fefbe1810cba38bcef0f6
ndiaye514m <ndiaye514m@gmail.com>
2024-11-13 11:56:03 +0000

    ci(webapp):add command test Ci13Nov sans erreur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'appRoot',
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
