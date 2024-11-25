12649a819aac6969a907404dd6cf93eb04552731
ndiaye514m <ndiaye514m@gmail.com>
2024-11-25 17:02:11 +0000

    ci(webapp):post deployment towards 3 env commented ProjectID etc mes_env2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '@mes_env/environment';
-//import { environment } from '../environments/environment';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
