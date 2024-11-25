964fb0fe931c623dec5aff48fe29c7124b2c1cd7
ndiaye514m <ndiaye514m@gmail.com>
2024-11-25 12:09:01 +0000

    ci(webapp):post deployment towards 3 env commented ProjectID etc with import env line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//import { environment } from '../environments/environment';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'productivity-planner';
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eeslint-disable-next-line @typescript-eslint/no-explicit-any
   //readonly isStaging=!(environment as any).production;
   //eadonly isStaging=environment.production;
  // isProductionEnvironment=environment.production;
