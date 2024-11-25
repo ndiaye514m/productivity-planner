cc8a6c55b0d09c7a17228926ba8ecdcb4bde1125
ndiaye514m <ndiaye514m@gmail.com>
2024-11-25 12:02:17 +0000

    ci(webapp):post deployment towards 3 env commented ProjectID etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
+//import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +12,9 @@ import { environment } from '../environments/environment';
 export class AppComponent {
   title = 'productivity-planner';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly isStaging=!(environment as any).production;
+  //readonly isStaging=!(environment as any).production;
   //eadonly isStaging=environment.production;
-  isProductionEnvironment=environment.production;
-  firebaseProjectId=environment.firebaseConfig.projectId;
+ // isProductionEnvironment=environment.production;
+  //firebaseProjectId=environment.firebaseConfig.projectId;
 
 }
