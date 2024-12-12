ed06dff03ddb39112fa1dfadc865e59413546bcf
ndiaye514m <ndiaye514m@gmail.com>
2024-12-12 21:02:30 +0000

    ci using environment variables from Github Secret and no Angular boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthenticationService } from './core/authentication.service';
-import { switchMap } from 'rxjs';
+
+
 
 
 @Component({
@@ -13,34 +13,8 @@ import { switchMap } from 'rxjs';
 })
 export class AppComponent {
 
-  title = 'productivity-planner';
-//test zone
-readonly #authenticationService =inject(AuthenticationService); 
+  //title = 'productivity-planner';
 
 
-onLogin(){
-  const email='ryan.gravenberch@gmail.com';
-  const password='passeroui';
-  this.#authenticationService.login(email,password).subscribe((response)=>console.log(response));
-  //this.#authenticationService.login('ryan.gravenberch@gmail.com','passeroui').subscribe((response)=>console.log(response));
-  //this.#authenticationService.login("demo@gmail.com","passer").subscribe((response)=>console.log(response));
-}
-
-onSave(){
-//const email='ryan.gravenberch@gmail.com';
-// const password='passeroui';
-const email='john.dalton@gmail.com';
-  const password='oepasser';
-  this.#authenticationService.login(email,password).pipe(
-    switchMap((response) => {
-      console.log(response);
-      const {email,localId, idToken}=response;
-      return this.#authenticationService.save(email,localId,idToken);
-    })
-  ).subscribe((response)=>console.log(response));
- // this.#authenticationService.login(email,password).subscribe((response)=>console.log(response));
-  //this.#authenticationService.login('ryan.gravenberch@gmail.com','passeroui').subscribe((response)=>console.log(response));
-  //this.#authenticationService.login("demo@gmail.com","passer").subscribe((response)=>console.log(response));
-}
 
 }
