d96ab7aa27db82c9381305105f17f785ea63e947
ndiaye514m <ndiaye514m@gmail.com>
2025-01-18 23:45:39 +0000

    feat(home):add page Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './visitor/home/home.page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        component: HomePageComponent, 
+        title:'Productivity Planner',
+    },
+];
