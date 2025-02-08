fc675d858d34d0091809bf44d9f9f941b4d8e05f
ndiaye514m <ndiaye514m@gmail.com>
2025-02-08 23:02:58 +0000

    feat(homebanner):add dumb component card list2

---
+++ src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.dumb.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { FeatureCardList } from './homepage-feature-card-list.type';
+
+@Component({
+  selector: 'app-homepage-feature-card-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage-feature-card-list.dumb.component.html',
+  styleUrl: './homepage-feature-card-list.dumb.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomepageFeatureCardListDumbComponent {
+
+  readonly featureCardList=input.required<FeatureCardList>();
+}
---
+++ src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.type.ts
@@ -0,0 +1,12 @@
+/*export type FeatureCard={
+    name:string;
+    icon:string;
+    description: string;
+}[];*/
+export interface FeatureCard{
+    name:string;
+    icon:string;
+    description: string;
+}[];
+
+export type FeatureCardList=FeatureCard[];
