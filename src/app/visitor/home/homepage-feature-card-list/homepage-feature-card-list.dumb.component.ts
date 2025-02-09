26d46a6e531f16536986b5c8e4a8fdb72e3215ab
ndiaye514m <ndiaye514m@gmail.com>
2025-02-09 22:58:38 +0000

    feat(homebanner):cards with icon done

--- src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.dumb.component.ts
+++ src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.dumb.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { FeatureCardList } from './homepage-feature-card-list.type';
+import { FeatureCardList } from './homepage-feature-card-list.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage-feature-card-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage-feature-card-list.dumb.component.html',
   styleUrl: './homepage-feature-card-list.dumb.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.interface.ts
+++ src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.interface.ts
@@ -1,12 +1,7 @@
-/*export type FeatureCard={
-    name:string;
-    icon:string;
-    description: string;
-}[];*/
 export interface FeatureCard{
     name:string;
     icon:string;
     description: string;
-}[];
+};
 
 export type FeatureCardList=FeatureCard[];
