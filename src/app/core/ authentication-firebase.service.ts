ca40fdcdb9d52a43568fa6846001382de9e81de4
ndiaye514m <ndiaye514m@gmail.com>
2025-03-03 12:45:16 +0000

    chore(env):env.dev to env

--- src/app/core/ authentication-firebase.service.ts
+++ src/app/core/ authentication-firebase.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.dev';
-//import { environment } from '../../environments/environment';
+//import { environment } from '../../environments/environment.dev';
+import { environment } from '../../environments/environment';
 import { map, Observable } from 'rxjs';
 import {
   AuthenticationService,
