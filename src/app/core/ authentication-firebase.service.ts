c6c96d6e9640c438a5a141915b942b2ba7e37d82
ndiaye514m <ndiaye514m@gmail.com>
2025-03-05 13:03:17 +0000

    chore(test): check if env file exists v16

--- src/app/core/ authentication-firebase.service.ts
+++ src/app/core/ authentication-firebase.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-// oui import { environment } from '../../environments/environment.dev';
 import { environment } from '../../environments/environment';
 import { map, Observable } from 'rxjs';
 import {
