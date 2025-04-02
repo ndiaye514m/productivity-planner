c04d209f1194befdcd522d52ec781899dd8c5061
ndiaye514m <ndiaye514m@gmail.com>
2025-04-02 21:02:28 +0000

    feature(login),test(signin and signup),refacto(ci jest env)

--- src/app/login/login.page.component.spec.ts
+++ src/app/login/login.page.component.spec.ts
@@ -1,15 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginPageComponent } from './login.page.component';
-import { DebugElement } from '@angular/core';
+//import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
-  let email: DebugElement;
-  let password: DebugElement;
+  /*let email: DebugElement;
+  let password: DebugElement;*/
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -68,28 +68,47 @@ describe('LoginPageComponent', () => {
 
         //Act
         email.nativeElement.value = '';
-       
         email.nativeElement.dispatchEvent(new Event('input'));
-       
         fixture.detectChanges();
-
         const error = fixture.debugElement.query(By.css('[data-testid="error-email-required"]'));
-       
-
-       
-
         const errorMessage = error.nativeElement.textContent;
+      
         //Assert
         expect(errorMessage).toContain('Email is required.');
-
         
       });
   
-      it.todo('should display error message when field do not contain a valid HTML5 email');
+      it('should display error message when field do not contain a valid HTML5 email',()=>{
+        //Arrange
+        const email = fixture.debugElement.query(By.css('[data-testid="email"]'));
+        //Act
+        email.nativeElement.value = 'invalid-email-exemple';
+        email.nativeElement.dispatchEvent(new Event('input'));
+        fixture.detectChanges();
+        const error = fixture.debugElement.query(By.css('[data-testid="error-email-pattern"]'));
+        const errorMessage = error.nativeElement.textContent;
+        
+        //Assert
+        expect(errorMessage).toContain('Email must be valid.');
+      });
     })
 
     describe('when user interact with password field', () => {
-      it.todo('should display error message when field is empty');
+      it('should display error message when field is empty',()=>{
+
+        //Arrange
+        const password = fixture.debugElement.query(By.css('[data-testid="password"]'));
+
+        //Act
+        password.nativeElement.value = '';
+        password.nativeElement.dispatchEvent(new Event('input'));
+        fixture.detectChanges();
+        const error = fixture.debugElement.query(By.css('[data-testid="error-password-required"]'));
+        const errorMessage = error.nativeElement.textContent;
+      
+        //Assert
+        expect(errorMessage).toContain('Password is required.');
+      });
     }); 
   
 
--- src/app/visitor/signup/signup.page.component.spec.ts
+++ src/app/visitor/signup/signup.page.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupPageComponent } from './signup.page.component';
-import { AuthenticationService } from '../../core/port/authentication.service';
-import { DebugElement } from '@angular/core';
 import { UserStore } from '../../core/store/user.store';
+import { AuthenticationService } from '../../core/port/authentication.service';
 import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+
 
 describe('SignupPageComponent', () => {
   let component: SignupPageComponent;
@@ -56,9 +58,49 @@ describe('SignupPageComponent', () => {
   })
 
 
+  describe('when user interact with name field', () => {
+    it('should display error message when field is empty', () => {
+      name.nativeElement.value = '';
+      name.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('[data-testid="error-name-required"]'));
+      const errorMessage = error.nativeElement.textContent;
+      expect(errorMessage).toBe('Name is required.');
+    });
+    it('should display error message when field contain less than 3 characters', () => {
+      name.nativeElement.value = 'a';
+      name.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+  
+      const error = fixture.debugElement.query(By.css('[data-testid="error-name-minlength"]'));
+      const errorMessage = error.nativeElement.textContent;
+      expect(errorMessage).toContain('Name must contain at least 3 caracters.');
+    });
+    it('should display error message when field contain more than 20 characters', () => {
+      name.nativeElement.value = 'abcdefghijklmnopqrstuvwxyz';
+      name.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+  
+      const error = fixture.debugElement.query(By.css('[data-testid="error-name-maxlength"]'));
+      const errorMessage = error.nativeElement.textContent;
+      
+      expect(errorMessage).toContain('Name must contain maximum 20 caracters.');
+    });
+    it('should display error message when field do not contain only letters', () => {
+      name.nativeElement.value = '!';
+      name.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+  
+      const error = fixture.debugElement.query(By.css('[data-testid="error-name-pattern"]'));
+      const errorMessage = error.nativeElement.textContent;
+      
+      expect(errorMessage).toContain('Name must contain only letters.');
+    });
+  });
+
+
   describe('when user interact with email field', () => {
-    it.todo('should display error message when field is empty');
-    it.todo('should display error message when field do not contain a valid HTML5 email');
+  
     it('should display error message when field is empty', () => {
       email.nativeElement.value = '';
       email.nativeElement.dispatchEvent(new Event('input'));
@@ -81,6 +123,126 @@ describe('SignupPageComponent', () => {
       expect(errorMessage).toContain('Email must be valid.');
     });
   });
+
+
+
+  describe('when user interact with password field', () => {
+   
+    it('should display error message when field is empty', () => {
+      password.nativeElement.value = '';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-required"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password is required.');
+    });
+
+    it('should display error message when field contain less than 8 characters', () => {
+      password.nativeElement.value = 'Ab1$e';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-minlength"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least 8 characters.');
+    });
+
+    it('should display error message when field do not contain at least 1 uppercase character', () => {
+      password.nativeElement.value = 'abc1$def';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-pattern"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character.');
+    });
+
+    it('should display error message when field do not contain at least 1 lowercase character', () => {
+      password.nativeElement.value = 'ABC1$DEF';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-pattern"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character.');
+    });
+
+    it('should display error message when field do not contain at least 1 digit', () => {
+      password.nativeElement.value = 'Abc$defg';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-pattern"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character.');
+    });
+
+    it('should display error message when field do not contain at least 1 of the following special characters ', () => {
+      password.nativeElement.value = 'Abc1defg';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-pattern"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character.');
+    });
+  });
+
+  describe('when user interact with "confirm password" field', () => {
+    it('should display error message when field is empty', () => {
+      confirmPassword.nativeElement.value = '';
+      confirmPassword.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('[data-testid="error-confirm-password-required"]'));
+      const errorMessage = error.nativeElement.textContent;
+      expect(errorMessage).toContain('Confirm password is required.');
+    });
+    it('should display error message when field do not have same value as password field', () => {
+      password.nativeElement.value = 'Abc1$def';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      confirmPassword.nativeElement.value = 'Different1$';
+      confirmPassword.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('[data-testid="error-confirm-password-match"]'));
+      const errorMessage = error.nativeElement.textContent;
+      expect(errorMessage).toContain('Passwords do not match.');
+    });
+  });
+
+
+
+  /*describe('when user submit the form', () => {
+    it('should register the user with form values', () => {
+      const userStore = TestBed.inject(UserStore);
+     
+      const spy = jest.spyOn(userStore,'register');
+      //const spy2 = spyOn(userStore, '');
+      name.nativeElement.value = 'John';
+      name.nativeElement.dispatchEvent(new Event('input'));
+      email.nativeElement.value = 'john@example.com';
+      email.nativeElement.dispatchEvent(new Event('input'));
+      password.nativeElement.value = 'Abc1$def';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      confirmPassword.nativeElement.value = 'Abc1$def';
+      confirmPassword.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      button.nativeElement.click();
+      fixture.detectChanges();
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith({
+        name: 'John',
+        email: 'john@example.com',
+        password: 'Abc1$def'
+      });
+    });
+  });*/
 
 });
 
