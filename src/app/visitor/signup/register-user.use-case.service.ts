de692330d60f1ade8d1b805c1607df2f337d55bc
ndiaye514m <ndiaye514m@gmail.com>
2025-04-09 22:32:06 +0000

    wip(signup):add signup workflow without error

--- src/app/visitor/signup/register-user.use-case.service.ts
+++ src/app/visitor/signup/register-user.use-case.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { User, Visitor } from 'src/app/core/entity/user.interface';
+import { AuthenticationService } from 'src/app/core/port/authentication.service';
+import { UserService } from 'src/app/core/port/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterUserUseCaseService {
 
-  constructor() { }
+  readonly #authenticationService=inject(AuthenticationService);
+  readonly #userService=inject(UserService);
+
+  async execute(visitor: Visitor): Promise<User|Error>{
+    // 1. Auhthenticate new visitor
+    const name = visitor.name;
+    const email=visitor.email;
+    const password=visitor.password;
+
+    const authResponse=await firstValueFrom(this.#authenticationService.register(email,password));
+    
+    // 2. Add credentials information in session storage
+    const jwtToken=authResponse.jwtToken;
+    const id=authResponse.userId;
+
+    
+    localStorage.setItem('jwtToken',jwtToken);
+    localStorage.setItem('email',email);
+
+    // 3. Create new user in database 
+    const user:User={
+      id: id,
+      name:name,
+      email:email
+    };
+
+    await firstValueFrom( this.#userService.create(user,jwtToken));
+
+    return user;
+  };
 }
