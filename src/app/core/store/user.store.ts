264320fe8d18cbe3c2d22dde6f02bd573b971698
ndiaye514m <ndiaye514m@gmail.com>
2025-04-14 13:37:32 +0000

    task(store): export signal store type

--- src/app/core/store/user.store.ts
+++ src/app/core/store/user.store.ts
@@ -8,7 +8,7 @@ export interface UserState {
     user: User | undefined;
   }
 
-    
+export type UserStore = InstanceType<typeof UserStore>;   
 export const UserStore = signalStore(
     // 👇 Providing `BooksStore` at the root level.
   { providedIn: 'root' },
