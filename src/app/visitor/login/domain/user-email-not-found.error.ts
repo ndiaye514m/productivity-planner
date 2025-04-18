df343a6a86c81eeae4c2048fe5e68af4e3ca1e4b
ndiaye514m <ndiaye514m@gmail.com>
2025-04-18 22:43:17 +0000

    Create user-email-not-found.error.ts

---
+++ src/app/visitor/login/domain/user-email-not-found.error.ts
@@ -0,0 +1,6 @@
+export class UserEmailNotFoundError extends Error {
+    constructor(readonly email: string) {
+      super(`The email ${email} does not exist. Try another email address or create a new account.`);
+      this.name = 'UserEmailNotFoundError';
+      }
+  }
